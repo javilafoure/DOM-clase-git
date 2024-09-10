42db9dee5529630052e536c4119c52fb2ca7869b
Joseph Antony Pierre Avila Foure - Tismart <extjpierre@belcorp.biz>
2024-09-10 15:49:49 -0500

    primer commit

---
+++ main.js
@@ -0,0 +1,41 @@
+const users = [
+    {
+        id: 1,
+        nombre: 'Ronnie',
+        apellido: 'Santoyo'
+    },
+    {
+        id: 2,
+        nombre: 'Augusto',
+        apellido: 'Santoyo'
+    },
+    {
+        id: 3,
+        nombre: 'William',
+        apellido: 'Santoyo'
+    },
+    {
+        id: 4,
+        nombre: 'Oliver',
+        apellido: 'Santoyo'
+    },
+    {
+        id: 5,
+        nombre: 'Harold',
+        apellido: 'Santoyo'
+    },
+
+]
+
+for (let i = 0; i < user.length; i++) {
+    console.log(users[i]['nombre'])
+}
+
+for (let i = 0; i < array.length; i++) {
+
+    if(users[1]['nombre'] !== 'Oliver'){
+        caja.innerHtml+="<li>" + nombre + "</li>"
+    }
+    
+    
+}
